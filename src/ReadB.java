68a5be21cd0451d5c9b690ff21a7845b86f371dd
giannisfourfouris <giannisfourfouris@gmail.com>
2018-12-16 20:14:20 +0200

    work but low accuracy

--- src/ReadB.java
+++ src/ReadB.java
@@ -1,26 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
+import static jdk.nashorn.internal.objects.NativeMath.round;
+
 public class ReadB {
     private HashMap<Integer, Double[]> MailHash = new HashMap<Integer, Double[]>();
     private HashSet<Integer> tempSet;
-    private int SpamCounter=0;
-    private int HammCounter=0;
+    private int SpamCounter = 0;
+    private int HammCounter = 0;
+
     private void inputToHashMap(String path) throws FileNotFoundException {
 
         for (int i = 1; i <= 9; i++) {
             String localPath = path + Integer.toString(i);
             File dir = new File(localPath);
             for (File file : dir.listFiles()) {
-                if(checkSpam(file))
-                {
+                if (checkSpam(file)) {
                     SpamCounter++;
-                }else
-                {
-                        HammCounter++;
+                } else {
+                    HammCounter++;
                 }
                 tempSet = new HashSet<Integer>();
                 Scanner scanner = new Scanner(file);
@@ -36,9 +38,9 @@ public class ReadB {
                         if (MailHash.containsKey(k)) {
 
                             MailHash.get(k)[0] += 1;
-                           // System.out.println("test: " + k + "getk0: " + MailHash.get(k)[0]);
+
                         } else {
-                            MailHash.put(k, new Double[]{0.0,0.0});
+                            MailHash.put(k, new Double[]{0.0, 0.0});
                             MailHash.get(k)[0] += 1;
                         }
                     }//end for
@@ -47,7 +49,7 @@ public class ReadB {
                         if (MailHash.containsKey(k)) {
                             MailHash.get(k)[1] += 1;
                         } else {
-                            MailHash.put(k, new Double[]{0.0,0.0});
+                            MailHash.put(k, new Double[]{0.0, 0.0});
                             MailHash.get(k)[1] += 1;
                         }
                     }//end for
@@ -60,52 +62,50 @@ public class ReadB {
             MailHash.get(lexi)[0]++;
             MailHash.get(lexi)[1]++;
         }
-        SpamCounter+=2;
-        HammCounter+=2;
+        SpamCounter += 2;
+        HammCounter += 2;
         Propability();
-        System.out.println("SpamCounter: "+ SpamCounter+" HammCounter: " + HammCounter);
+        System.out.println("SpamCounter: " + SpamCounter + " HammCounter: " + HammCounter);
     }
-    private boolean checkSpam (File dir) throws FileNotFoundException
-    {
+
+    private boolean checkSpam(File dir) throws FileNotFoundException {
         if (dir.getName().contains("spmsg")) {
             return true;
         }
         return false;
     }
-    private  void Propability()
-    {
-        for(int lexi: MailHash.keySet())
-        {
-            MailHash.get(lexi)[0]= Math.log(MailHash.get(lexi)[0]/SpamCounter);
-            MailHash.get(lexi)[1]= Math.log(MailHash.get(lexi)[1]/HammCounter);
+
+    private void Propability() {
+        for (int lexi : MailHash.keySet()) {
+            MailHash.get(lexi)[0] = Math.pow(Math.log(MailHash.get(lexi)[0] / SpamCounter),2);
+            MailHash.get(lexi)[1] = Math.pow(Math.log(MailHash.get(lexi)[1] / HammCounter),2);
         }
     }
 
-    public void ReadMail() throws FileNotFoundException{
+    public void ReadMail() throws FileNotFoundException {
         String path = "src\\pu_corpora_public\\pu1\\part";
-
         inputToHashMap(path);
-//        for (int word: MailHash.keySet()){
-//            System.out.println(word + " " +"spam is: "+MailHash.get(word)[0]+" ham is: "+MailHash.get(word)[1]);
-//        }
+        /*for (int word : MailHash.keySet()) {
+            System.out.println(word + " " + "spam is: " + MailHash.get(word)[0] + " ham is: " + MailHash.get(word)[1]);
+        }*/
         CalculateTest(path);
     }
-    public double getSpamPropability()
-    {
-        return SpamCounter/(SpamCounter+HammCounter);
+
+    public double getSpamPropability() {
+        return SpamCounter / (SpamCounter + HammCounter);
     }
-    public double getHamPropability()
-    {
-        return 1-getSpamPropability();
+
+    public double getHamPropability() {
+        return 1 - getSpamPropability();
     }
+
     public void CalculateTest(String path) throws FileNotFoundException {
         String localPath = path + Integer.toString(10);
         File dir = new File(localPath);
-
-        for (File file : dir.listFiles())
-        {
-            double SpamPropability=1.0;
-            double HamPropability=1.0;
+        int counterMessage=0;
+        int correctCounterMessage=0;
+        for (File file : dir.listFiles()) {
+         counterMessage++;
             tempSet = new HashSet<Integer>();
             Scanner scanner = new Scanner(file);
             while (scanner.hasNext()) {
@@ -115,30 +115,44 @@ public class ReadB {
                     tempSet.add(cell);
                 }
             }//end while
-            for(int word : tempSet)
-            {
-                if(MailHash.containsKey(word))
-                {
-                    SpamPropability=SpamPropability*MailHash.get(word)[0];
-                    HamPropability=HamPropability*MailHash.get(word)[1];
+            double SpamPropability = 1.0;
+            double HamPropability = 1.0;
+
+
+            for (int word : tempSet) {
+                if (MailHash.containsKey(word)) {
+
+                    SpamPropability = SpamPropability + MailHash.get(word)[0];
+                    HamPropability = HamPropability + MailHash.get(word)[1];
+
+
+                } else {
+                    SpamPropability = SpamPropability + Math.pow(Math.log(1/SpamCounter),2);
+                    HamPropability = HamPropability + Math.pow(Math.log(1/HammCounter),2);
+
                 }
-                else
-                {
-                    SpamPropability=SpamPropability*(1/SpamCounter);
-                    HamPropability=HamPropability*(1/HammCounter);
-                }
-                System.out.println("word: " +word +" spam Propability: "+ SpamPropability + " hamPropability: " + HamPropability );
+              //  System.out.println("Word: " + word + " spam " + SpamPropability + " ham " + HamPropability);
+
             }
-            if(SpamPropability*getSpamPropability()<= HamPropability*getHamPropability())
-            {
-                System.out.println("Hamm " + file.toString() + " hameprp: " + HamPropability*getHamPropability() + " spam: " + SpamPropability*getSpamPropability());
-            }
-            else
-                {
-                    System.out.println("Spam" + file.toString() + " hameprp: " + HamPropability*getHamPropability() + " spam: " + SpamPropability*getSpamPropability());
+
+            if (SpamPropability * getSpamPropability() <= HamPropability * getHamPropability()) {
+                System.out.println("Its a Ham eimai sto file " + file.toString());
+                if(!checkSpam(file)){
+                   correctCounterMessage++;
                 }
+            } else {
+                System.out.println("Its a Spam eimai sto file " + file.toString());
+                if(checkSpam(file)){
+                    correctCounterMessage++;
+                }
+            }
+
         }//end for
 
+         System.out.println(correctCounterMessage);
+         System.out.println(counterMessage);
+         double d =(double) correctCounterMessage/(double) counterMessage;
+         System.out.println(d*100);
     }
 }
 
