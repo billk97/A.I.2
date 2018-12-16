ad0637dc3ff4aec6dfb83187d0ce6b77874a8eab
YoungAbuDhabi <mlsalex13@gmail.com>
2018-12-16 15:21:48 +0200

    readb

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,20 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException
     {
-        Read read =new Read();
-        read.ReadFile();
+       // Read read =new Read();
+       // read.ReadFile();
+
+        ReadB readb=new ReadB();
+        readb.ReadMail();
+
+
+
     }
 
 }
