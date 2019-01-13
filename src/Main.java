366b72919875ed3f4d2439435738c270cef6fafb
giannisfourfouris <giannisfourfouris@gmail.com>
2019-01-13 19:11:38 +0200

    Fix Main

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,18 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException
     {
         //this is for Bayes
-       //TestBayes testBayes = new TestBayes();
-      //testBayes.WriteTxt();
+       TestBayes testBayes = new TestBayes();
+      testBayes.WriteTxt();
 
         //this is for ID3
-//        PrepereID3Data id=new PrepereID3Data();
-//        id.initializeData();
-//        Id3Data id3Data =new Id3Data();
-//        id3Data.Initializer(id.getMailCounter(),id.TrainingDataNumber,id.path);
-        LogisticRegression logistic = new LogisticRegression();
-        logistic.initializeData();
+     /*   PrepereID3Data id=new PrepereID3Data();
+        id.initializeData();
+        Id3Data id3Data =new Id3Data();
+        id3Data.Initializer(id.getMailCounter(),id.TrainingDataNumber,id.path);*/
+
+        //this is for LogisticRegression
+      /*  LogisticRegression logistic = new LogisticRegression();
+        logistic.initializeData();*/
 
 
 
